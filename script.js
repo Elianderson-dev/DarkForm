36286e4c2c5b2565209594703cefb2198f0f4410
elianderson-dev <nin2333124568@gmail.com>
2026-02-04 23:45:57 -0300

    feat: adds CREATE operation

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const itemInput = document.getElementById('addItem');
+const send = document.getElementById('send');
 const form = document.getElementById('form');
+const itemsList = document.getElementById('items');
+
+// CREATE
+form.addEventListener('submit', function(addItem) {
+    addItem.preventDefault();
+    const newItemText = itemInput.value.trim();
+    if (newItemText !== '') {
+        const li = document.createElement('li');
+        li.className = 'listGroupItem';
+        li.appendChild(document.createTextNode(newItemText));
+        itemsList.appendChild(li);
+        
+        // Clear input field after writing
+        itemInput.value = '';
+    }
+});
