d4cdb0f4b2e4a3bf48847dfe937207595ff31f88
elianderson-dev <nin2333124568@gmail.com>
2026-02-06 00:44:42 -0300

    refactor: changes object, array and id organization on CREATE operation

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ const send = document.getElementById('send');
 const form = document.getElementById('form');
 const itemsList = document.getElementById('items');
 
+// Basic structure for in-memory storage
+
+let items = [];
+let currentId = 1;
+
 // CREATE
 form.addEventListener('submit', function(addItem) {
     addItem.preventDefault();
     const newItemText = itemInput.value.trim();
     if (newItemText !== '') {
-        const li = document.createElement('li');
-        li.className = 'listGroupItem';
-        li.appendChild(document.createTextNode(newItemText));
-        itemsList.appendChild(li);
         
+        const item = {
+            id: currentId++,
+            element: newItemText
+        }
+
+        items.push(item)
+
         // Clear input field after writing
         itemInput.value = '';
+
+        renderItems();
     }
 }); 
 
-// READ
 
+// READ 
 
 // UPDATE
 
